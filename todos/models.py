4175ab977381f7e22ac64611ba85a56071e77fa1
kim <zbqlxm5484@naver.com>
2026-05-12 17:41:21 +0900

    fix boolean default

--- todos/models.py
+++ todos/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    is_completed = models.BooleanField()
+    is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
